28ed5e12efc05d2156e37fdf71e541a4e24773d6
Leonardo Maccari <leonardo.maccari@unitn.it>
2017-11-20 12:33:22 +0100

    comments in main

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
 #! /usr/bin/env python
 
+"""
+A simple main documenting how to use thhis python package
+"""
+
+
 from netdiff import NetJsonParser
 from netdiff.utils import _netjson_networkgraph as to_netjson
 from netjsonanalyser import ParsedGraph
 import sys
 import json
 
-NetJSON = sys.argv[1]
 
-nj = NetJsonParser(file=NetJSON)
+NetJSON = sys.argv[1]  # a NetJSON graph file
+
+nj = NetJsonParser(NetJSON)
 pg = ParsedGraph(nj)
 pg.condensate_graph()
 js = to_netjson(pg.netJSON.protocol, pg.netJSON.version, pg.netJSON.revision,
